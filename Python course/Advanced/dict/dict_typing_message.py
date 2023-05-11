9ed2d881065dcf2c0f73aeb11c67ef381fa00005
user <yasenovich.mikhail@mail.ru>
2023-05-11 13:31:01 +0300

    completed get_typing_combination function

--- Python course/Advanced/dict/dict_typing_message.py
+++ Python course/Advanced/dict/dict_typing_message.py
@@ -1,5 +1,6 @@
 def get_typing_combination(text, numbers):
     my_list = list(text.upper())
+    result = ''
     print(my_list)
     for i in my_list:
         # print(i)
@@ -8,11 +9,14 @@ def get_typing_combination(text, numbers):
                 if i in value:
                     # print(key)
                     for j in range(len(value)):
-                        if i != value[j]:
-                            print(key)
+                        result += key
+                        if i == value[j]:
+                            break
+    print(result)
+                        
 
-numbers = {1: '.,?!:', 2: 'ABC', 3: 'DEF', 4: 'GHI', 5: 'JKL',
- 6: 'MNO', 7: 'PQRS', 8: 'TUV', 9: 'WXYZ', 0: ' '}
+numbers = {'1': '.,?!:', '2': 'ABC', '3': 'DEF', '4': 'GHI', '5': 'JKL',
+ '6': 'MNO', '7': 'PQRS', '8': 'TUV', '9': 'WXYZ', '0': ' '}
 
 text = 'Hello, World!'
 
