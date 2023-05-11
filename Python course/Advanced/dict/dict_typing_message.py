7d626df50fd5f707db1fef2d71f11b548c841025
user <yasenovich.mikhail@mail.ru>
2023-05-11 12:43:16 +0300

    fixed output needed number

--- Python course/Advanced/dict/dict_typing_message.py
+++ Python course/Advanced/dict/dict_typing_message.py
@@ -5,12 +5,11 @@ def get_typing_combination(text, numbers):
         # print(i)
         for key, value in numbers.items():
             # print(value)
-            for j in range(len(value)):
-                # print(value[j])
-                if i == value[j]:
-                    print(key)
-            # if i in value:
-                # print(key)
+                if i in value:
+                    # print(key)
+                    for j in range(len(value)):
+                        if i != value[j]:
+                            print(key)
 
 numbers = {1: '.,?!:', 2: 'ABC', 3: 'DEF', 4: 'GHI', 5: 'JKL',
  6: 'MNO', 7: 'PQRS', 8: 'TUV', 9: 'WXYZ', 0: ' '}
