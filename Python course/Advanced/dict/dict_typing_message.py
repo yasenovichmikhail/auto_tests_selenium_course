b1f6b96238b1f1fd3189251a59214eb49d067784
user <yasenovich.mikhail@mail.ru>
2023-05-11 12:20:24 +0300

    added get_typing_message function. Added first two loops

---
+++ Python course/Advanced/dict/dict_typing_message.py
@@ -0,0 +1,20 @@
+def get_typing_combination(text, numbers):
+    my_list = list(text.upper())
+    print(my_list)
+    for i in my_list:
+        # print(i)
+        for key, value in numbers.items():
+            # print(value)
+            for j in range(len(value)):
+                # print(value[j])
+                if i == value[j]:
+                    print(key)
+            # if i in value:
+                # print(key)
+
+numbers = {1: '.,?!:', 2: 'ABC', 3: 'DEF', 4: 'GHI', 5: 'JKL',
+ 6: 'MNO', 7: 'PQRS', 8: 'TUV', 9: 'WXYZ', 0: ' '}
+
+text = 'Hello, World!'
+
+get_typing_combination(text, numbers)
