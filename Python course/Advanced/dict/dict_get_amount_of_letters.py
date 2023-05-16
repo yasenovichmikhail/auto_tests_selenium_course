0a69f895e347db650c4c173488b7ca7ca6c1790c
user <yasenovich.mikhail@mail.ru>
2023-05-16 10:03:11 +0300

    added dictionary get_amount_of_letters function

---
+++ Python course/Advanced/dict/dict_get_amount_of_letters.py
@@ -0,0 +1,29 @@
+
+def get_all_dogs(pets):
+    result = {}
+    my_list = []
+    list_pets = []
+    for i in pets:
+        my_list.append(i[1:])
+        list_pets.append(i[0])
+    for key, value in zip(list_pets, my_list):
+        result.setdefault(value, []).append(key)
+    print(result)
+
+pets = [('Hatiko', 'Parker', 'Wilson', 50),
+        ('Rusty', 'Josh', 'King', 25),
+        ('Fido', 'John', 'Smith', 28),
+        ('Butch', 'Jake', 'Smirnoff', 18),
+        ('Odi', 'Emma', 'Wright', 18),
+        ('Balto', 'Josh', 'King', 25),
+        ('Barry', 'Josh', 'King', 25),
+        ('Snape', 'Hannah', 'Taylor', 40),
+        ('Horry', 'Martha', 'Robinson', 73),
+        ('Giro', 'Alex', 'Martinez', 65),
+        ('Zooma', 'Simon', 'Nevel', 32),
+        ('Lassie', 'Josh', 'King', 25),
+        ('Chase', 'Martha', 'Robinson', 73),
+        ('Ace', 'Martha', 'Williams', 38),
+        ('Rocky', 'Simon', 'Nevel', 32)]
+
+get_all_dogs(pets)
