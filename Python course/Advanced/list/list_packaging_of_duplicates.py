1332f3de3d8cfa850bb8ac70a9b3ec82e9a08b30
user <yasenovich.mikhail@mail.ru>
2023-05-22 10:07:09 +0300

    created list directory and get_packaging_of_duplicates function

---
+++ Python course/Advanced/list/list_packaging_of_duplicates.py
@@ -0,0 +1,20 @@
+
+def get_packaging_of_duplicates(s):
+    list = s.split(' ')
+    result = []
+    print(list)
+    for i in range(len(list)):
+        if [list[i]] not in result:
+            result.append([list[i]])
+        else:
+            while [list[i]] == result[-1]:
+                print([list[i]])
+                print(result[-1])
+                result[-1].append(list[i])
+                # print(result[-1])
+                print(result)
+
+
+s = 'w w w o r l d g g g g r e a t t e c c h e m g g p w w'
+
+get_packaging_of_duplicates(s)
