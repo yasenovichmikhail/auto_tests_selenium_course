ae20727fb56ca590a272d0c59e3d0be0ed2b43ee
user <yasenovich.mikhail@mail.ru>
2023-05-23 14:55:19 +0300

    added dictionary generator examples

---
+++ Python course/Advanced/dict/dict_generator_examples.py
@@ -0,0 +1,33 @@
+
+numbers = [34, 10, -4, 6, 10, 23, -90, 100, 21, -35, -95, 1, 36, -38, -19, 1, 6, 87]
+result = {i: numbers[i]**2 for i in range(len(numbers))}
+
+colors = {'c1': 'Red', 'c2': 'Grey', 'c3': None, 'c4': 'Green', 'c5': 'Yellow', 'c6': 'Pink', 'c7': 'Orange', 'c8': None, 'c9': 'White', 'c10': 'Black', 'c11': 'Violet', 'c12': 'Gold', 'c13': None, 'c14': 'Amber', 'c15': 'Azure', 'c16': 'Beige', 'c17': 'Bronze', 'c18': None, 'c19': 'Lilac', 'c20': 'Pearl', 'c21': None, 'c22': 'Sand', 'c23': None}
+result1 = {key: colors[key] for key, value in colors.items() if value != None}
+
+favorite_numbers = {'timur': 17, 'ruslan': 7, 'larisa': 19, 'roman': 123, 'rebecca': 293, 'ronald': 76, 'dorothy': 62, 'harold': 36, 'matt': 314, 'kim': 451, 'rosaly': 18, 'rustam': 89, 'soltan': 111, 'amir': 654, 'dima': 390, 'amiran': 777, 'geor': 999, 'sveta': 75, 'rita': 909, 'kirill': 404, 'olga': 271, 'anna': 55, 'madlen': 876}
+result2 = {key: favorite_numbers[key] for key, value in favorite_numbers.items() if 9 < value < 99}
+
+months = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
+result3 = {value: key for key, value in months.items()}
+
+s = '1:men 2:kind 90:number 0:sun 34:book 56:mountain 87:wood 54:car 3:island 88:power 7:box 17:star 101:ice'
+result4 = {int(i.split(':')[0]): i.split(':')[1] for i in s.split(' ')}
+
+numbers = [1, 6, 18]
+result5 = {i: [j for j in range(1, i + 1) if i % j == 0] for i in numbers}
+
+words = ['yes', 'hello']
+result6 = {i: [ord(i[j]) for j in range(len(i))] for i in words}
+
+letters = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 26: 'Z'}
+remove_keys = [1, 5, 7, 12, 17, 19, 21, 24]
+result7 = {key: value for key, value in letters.items() if key not in remove_keys}
+
+students = {'Timur': (170, 75), 'Ruslan': (180, 105), 'Soltan': (192, 68), 'Roman': (175, 70), 'Madlen': (160, 50), 'Stefani': (165, 70), 'Tom': (190, 90), 'Jerry': (180, 87), 'Anna': (172, 67), 'Scott': (168, 78), 'John': (186, 79), 'Alex': (195, 120), 'Max': (200, 110), 'Barak': (180, 89), 'Donald': (170, 80), 'Rustam': (186, 100), 'Alice': (159, 59), 'Rita': (170, 80), 'Mary': (175, 69), 'Jane': (190, 80)}
+result8 = {key: value for key, value in students.items() if int(value[0]) > 167 and int(value[1]) < 75}
+
+tuples = [(1, 2, 3), (4, 5, 6), (7, 8, 9), (10, 11, 12), (13, 14, 15), (16, 17, 18), (19, 20, 21), (22, 23, 24), (25, 26, 27), (28, 29, 30), (31, 32, 33), (34, 35, 36)]
+result = {i[0]: i[1:] for i in tuples}
+
+print(result)
