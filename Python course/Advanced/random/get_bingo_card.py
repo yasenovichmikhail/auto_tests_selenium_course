4c24571f92d8c6e90ed8c95887c8e92dd2759e98
user <yasenovich.mikhail@mail.ru>
2023-05-29 09:49:37 +0300

    fixed problev with zero in the middle

--- Python course/Advanced/random/get_bingo_card.py
+++ Python course/Advanced/random/get_bingo_card.py
@@ -5,22 +5,34 @@ unique_numbers = []
 
 def get_one_line():
     my_str = ''
-    for i in range(5):
+    line = []
+    while len(line) != 5:
         rnd = random.randint(1, 75)
-        my_str += str(rnd).ljust(3)
+        if rnd not in unique_numbers:
+            my_str += str(rnd).ljust(3)
+            unique_numbers.append(rnd)
+            line.append(rnd)
+        else:
+            continue
     print(my_str)
 
 def get_one_line_zero():
     my_str = ''
     my_list = []
-    for i in range(5):
+    line = []
+    while len(line) != 5:
         rnd = random.randint(1, 75)
-        my_list.append(rnd)
         # print(len(my_list))
-        if len(my_list) == 3:
-            my_str += '0'.ljust(3)
+        if rnd not in unique_numbers:
+            unique_numbers.append(rnd)
+            line.append(rnd)
+            my_list.append(rnd)
+            if len(my_list) == 3:
+                my_str += '0'.ljust(3)
+            else:
+                my_str += str(rnd).ljust(3)
         else:
-            my_str += str(rnd).ljust(3)
+            continue
     print(my_str)
 
 def get_bingo_card():
