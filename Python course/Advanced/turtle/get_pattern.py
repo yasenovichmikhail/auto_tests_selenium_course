5cba3e16469e77f6d0ae41371f3296fd05703561
user <yasenovich.mikhail@mail.ru>
2023-06-02 17:23:50 +0300

    added get_pattern function

---
+++ Python course/Advanced/turtle/get_pattern.py
@@ -0,0 +1,34 @@
+import turtle
+window = turtle.Screen()
+
+def triangle_left():
+    for i in range(3):
+        turtle.forward(200)
+        turtle.left(120)
+
+def triangle_right():
+    for i in range(3):
+        turtle.forward(200)
+        turtle.right(120)
+
+circle = [(100, -100), (0, 70), (200, 70)]
+triangle_left()
+
+def get_circle():
+    for j in circle:
+        turtle.penup()
+        turtle.goto(j)
+        turtle.pendown()
+        turtle.fillcolor('black')
+        turtle.begin_fill()
+        turtle.circle(30)
+
+get_circle()
+
+turtle.penup()
+turtle.goto(0, 100)
+turtle.pendown()
+
+triangle_right()
+
+window.exitonclick()
