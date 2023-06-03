19481c5935b9d62181b071ae2dcdb3727ea8afaf
unknown <yasenovich.mikhail@mail.ru>
2023-06-03 11:09:04 +0300

    added draw_drotted_line function

---
+++ Python course/Advanced/turtle/draw_dotted_line.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.pensize(5)
+
+def get_drotted_line():
+  for i in range(11):
+      turtle.dot()
+      turtle.penup()
+      turtle.forward(20)
+      turtle.pendown
+
+get_drotted_line()
