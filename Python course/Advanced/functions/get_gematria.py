7dd5c98013448c45614d08987f461f493fdbabb6
unknown <yasenovich.mikhail@mail.ru>
2023-07-03 11:03:29 +0300

    added get_gematria function

---
+++ Python course/Advanced/functions/get_gematria.py
@@ -0,0 +1,6 @@
+def gematria(word):
+    return sum(map(lambda x: ord(x.upper()) - ord('A'), word)), word
+
+words = [input() for _ in range(int(input()))]
+
+print(*sorted(words, key=gematria), sep='\n')
