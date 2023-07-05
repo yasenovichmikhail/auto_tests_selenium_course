023f49d6287fe15f704ff44582f7b848dc091a98
user <yasenovich.mikhail@mail.ru>
2023-07-05 09:46:25 +0300

    added get_filtered_sorted_words function

---
+++ Python course/Advanced/functions/get_filtered_sorted_words.py
@@ -0,0 +1,8 @@
+words = ['beverage', 'monday', 'abroad', 'bias', 'abuse', 'abolish', 'abuse', 'abuse', 'bid', 'wednesday', 'able', 'betray', 'accident', 'abduct', 'bigot', 'bet', 'abandon', 'besides', 'access', 'friday', 'bestow', 'abound', 'absent', 'beware', 'abundant', 'abnormal', 'aboard', 'about', 'accelerate', 'abort', 'thursday', 'tuesday', 'sunday', 'berth', 'beyond', 'benevolent', 'abate', 'abide', 'bicycle', 'beside', 'accept', 'berry', 'bewilder', 'abrupt', 'saturday', 'accessory', 'absorb']
+
+filtered_words = list(filter(lambda x: len(x) == 6, words))
+
+sorted_words = sorted(filtered_words)
+
+# print(filtered_words)
+print(*sorted_words)
