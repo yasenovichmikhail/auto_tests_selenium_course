cefd9e8f071eff048c02a499d279ce75997674de
user <yasenovich.mikhail@mail.ru>
2023-09-05 15:46:00 +0300

    hot fix

--- Python course/Advanced/list/list_packaging_of_duplicates.py
+++ Python course/Advanced/list/list_packaging_of_duplicates.py
@@ -1,18 +1,14 @@
+res = []
 
 def get_packaging_of_duplicates(s):
-    list = s.split(' ')
-    result = [[]]
-    print(list)
-    for i in range(len(list)):
-        if [list[i]] not in result:
-            result.append([list[i]])
+    s = s.split()
+    for el in s:
+        if res and el in res[-1]:
+            res[-1].append(el)
         else:
-            while [list[i]] == result[-1]:
-                print([list[i]])
-                print(result[-1])
-                result[-1].append(list[i])
-                # print(result[-1])
-                print(result)
+            res.append([el])
+
+    print(res)
 
 
 s = 'w w w o r l d g g g g r e a t t e c c h e m g g p w w'
