4ab9efa76359b65905087e00f132b3aceda81fb0
user <yasenovich.mikhail@mail.ru>
2023-10-04 17:20:33 +0300

    added matplotlib pie with legend

---
+++ Python course/Advanced/matplotlib/pie_with_legend.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+from matplotlib import rc  # для вывода русских букв
+ 
+font = {'family': 'Verdana', # для вывода русских букв
+        'weight': 'normal'}
+       
+rc('font', **font)
+
+y = np.array([35, 25, 25, 15])
+
+
+# метки диаграммы
+mylabels = ["Яблоки", "Бананы", "Вишня", "Финики"]
+
+plt.pie(y, labels = mylabels)
+plt.show()
