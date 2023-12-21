cd6593fa948b84601eeb8f70d55e16478202dc10
unknown <yasenovich.mikhail@mail.ru>
2023-12-21 11:53:45 +0300

    added multiprocessing module

---
+++ Python course/Advanced/multiprocessing/multiprocessing_example.py
@@ -0,0 +1,29 @@
+from multiprocessing import Process
+import os
+import time
+
+def square_numbers():
+    for i in range(100):
+        i * i
+        time.sleep(0.1)
+
+processes = []
+
+num_processes = os.cpu_count()
+
+#create process
+
+for i in range(num_processes):
+    p = Process(target=square_numbers)
+    processes.append(p)
+
+#start
+for p in processes:
+    p.start()
+
+#join
+for p in processes:
+    p.join()
+
+print('End main')
+
