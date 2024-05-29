887c848f00e7cd05637ea4553d064d78c2730be9
user <yasenovich.mikhail@mail.ru>
2024-05-29 16:38:22 +0300

    added logger example

--- Python course/Advanced/logging/logging_example.py
+++ Python course/Advanced/logging/logging_example.py
@@ -1,10 +1,25 @@
 import logging
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    datefmt='%m/%d/%Y %H:%M:%S')
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+#                     datefmt='%m/%d/%Y %H:%M:%S')
 
-logging.debug('This is a debug message')
-logging.info('This is an info message')
-logging.warning('This is a warning message')
-logging.error('This is an error message')
-logging.critical('This is a critical message')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+console_handler.setFormatter(formatter)
+
+file_handler = logging.FileHandler('app.log')
+file_handler.setLevel(logging.DEBUG)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+logger.addHandler(file_handler)
+
+logger.debug('This is a debug message')
+logger.info('This is a info message')
+logger.warning('This is a warning message')
+logger.error('This is a error message')
+logger.critical('This is a critical message')
