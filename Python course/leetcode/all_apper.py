e8eb69845867f73579b0ae379be386c8de941235
user <yasenovich.mikhail@mail.ru>
2024-06-17 17:23:47 +0300

    added leetcode tasks

---
+++ Python course/leetcode/all_apper.py
@@ -0,0 +1,9 @@
+
+def to_apper(s):
+    res = s.title()
+    return res
+
+
+s = "they're bill's    friends    from    the    UK"
+
+print(to_apper(s))
---
+++ Python course/leetcode/is_palindrom.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        x = str(x)
+        if x[:] == x[::-1]:
+            return True
+        else:
+            return False
+
+
+s = Solution()
+print(s.isPalindrome(121))
