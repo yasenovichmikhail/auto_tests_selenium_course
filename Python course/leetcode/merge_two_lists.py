566665193b1e1e592d2af79d36dac95a71766d91
user <yasenovich.mikhail@mail.ru>
2024-06-20 17:26:59 +0300

    added merge_two_lists_tasks

---
+++ Python course/leetcode/merge_two_lists.py
@@ -0,0 +1,7 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        list3 = list1 + list2
+        print(list3)
+
+s = Solution()
+s.mergeTwoLists(list1, list2)
