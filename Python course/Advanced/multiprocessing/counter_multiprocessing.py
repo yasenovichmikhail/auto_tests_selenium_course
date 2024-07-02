d16ff1d65c42fdb2bd4777c651cb57797a5c9b4a
user <yasenovich.mikhail@mail.ru>
2024-07-02 16:53:13 +0300

    added multiprocessing_counter example

--- Python course/Advanced/multiprocessing/counter_multiprocessing.py
+++ Python course/Advanced/multiprocessing/counter_multiprocessing.py
@@ -9,32 +9,20 @@ def counter(num):
 def main():
     print(cpu_count())
     
-    a = Process(target=counter, args=(12500000,))
-    b = Process(target=counter, args=(12500000,))
-    c = Process(target=counter, args=(12500000,))
-    d = Process(target=counter, args=(12500000,))
-    e = Process(target=counter, args=(12500000,))
-    f = Process(target=counter, args=(12500000,))
-    g = Process(target=counter, args=(12500000,))
-    k = Process(target=counter, args=(12500000,))
+    a = Process(target=counter, args=(25000000,))
+    b = Process(target=counter, args=(25000000,))
+    c = Process(target=counter, args=(25000000,))
+    d = Process(target=counter, args=(25000000,))
 
     a.start()
     b.start()
     c.start()
     d.start()
-    e.start()
-    f.start()
-    g.start()
-    k.start()
 
     a.join()
     b.join()
     c.join()
     d.join()
-    e.join()
-    f.join()
-    g.join()
-    k.join()
     print("finished in:", time.perf_counter(), "seconds")
 
 if __name__ == '__main__':
