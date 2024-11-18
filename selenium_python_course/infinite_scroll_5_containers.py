7a2ec6af46c2f1eadff3fbd9973d90a636f07ad2
user <yasenovich.mikhail@mail.ru>
2024-11-18 13:54:21 +0300

    added infinite_scroll_5_containers script

--- selenium_python_course/infinite_scroll_5_containers.py
+++ selenium_python_course/infinite_scroll_5_containers.py
@@ -13,7 +13,45 @@ from selenium.webdriver.common.by import By
 
 with webdriver.Chrome() as driver:
     driver.get('https://parsinger.ru/infiniti_scroll_3/')
-    container_1 = driver.find_element(By.XPATH, "//*[@id='scroll-container_1']/div")
     actions = ActionChains(driver)
-    for i in range(5):
-        actions.move_to_element(container_1).scroll_by_amount(0, 100).perform()
+    container1 = []
+    container2 = []
+    container3 = []
+    container4 = []
+    container5 = []
+    total = 0
+    while len(container5) != 100:
+        for i in range(1, 6):
+            container = driver.find_element(By.XPATH, f"//div[@id='scroll-container_{i}']")
+            container_content = container.text.split()
+            scroll_container = driver.find_element(By.XPATH, f"//*[@id='scroll-container_{i}']/div")
+            if i == 1:
+                for digit in container_content:
+                    if digit not in container1:
+                        container1.append(digit)
+                        total += int(digit)
+            elif i == 2:
+                for digit in container_content:
+                    if digit not in container2:
+                        container2.append(digit)
+                        total += int(digit)
+            elif i == 3:
+                for digit in container_content:
+                    if digit not in container3:
+                        container3.append(digit)
+                        total += int(digit)
+            elif i == 4:
+                for digit in container_content:
+                    if digit not in container4:
+                        container4.append(digit)
+                        total += int(digit)
+            elif i == 5:
+                for digit in container_content:
+                    if digit not in container5:
+                        container5.append(digit)
+                        total += int(digit)
+            actions.move_to_element(scroll_container).scroll_by_amount(0, 100).perform()
+    print(total)
+
+
+
