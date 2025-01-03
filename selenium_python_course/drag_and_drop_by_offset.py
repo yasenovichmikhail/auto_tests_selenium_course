17bae1406e128ed7f6bbc6a6d4c1cb6d833accdd
unknown <yasenovich.mikhail@mail.ru>
2025-01-03 17:03:33 +0300

    modified drag_aand_drop script

--- selenium_python_course/drag_and_drop_by_offset.py
+++ selenium_python_course/drag_and_drop_by_offset.py
@@ -5,17 +5,14 @@ from selenium.webdriver.common.by import By
 
 url = "https://parsinger.ru/selenium/5.10/8/index.html"
 
+all_stages = [i + 100 for i in range(0, 800, 100)]
+
 with webdriver.Chrome() as driver:
     driver.get(url)
     actions = ActionChains(driver=driver)
-    red_box = driver.find_element(By.XPATH, "//div[@id='draggable']")
-    target1 = driver.find_element(By.XPATH, "//div[@id='box1']")
-    target2 = driver.find_element(By.XPATH, "//div[@id='box2']")
-    target3 = driver.find_element(By.XPATH, "//div[@id='box3']")
-    target4 = driver.find_element(By.XPATH, "//div[@id='box4']")
-    actions.drag_and_drop(red_box, target1).perform()
-    actions.drag_and_drop(red_box, target2).perform()
-    actions.drag_and_drop(red_box, target3).perform()
-    actions.drag_and_drop(red_box, target4).perform()
-    res = driver.find_element(By.XPATH, "//div[@id='message']").text
+    for stage in all_stages:
+        box = driver.find_element(By.XPATH, f"//div[@id='piece_{stage}']")
+        target = driver.find_element(By.XPATH, f"//div[@id='range_{stage}']")
+        actions.drag_and_drop(source=box, target=target).perform()
+        res = driver.find_element(By.XPATH, "//p[@id='message']").text
     print(res)
