4eab8602d627cc7fe402ddde261ef9b6b731842b
user <yasenovich.mikhail@mail.ru>
2025-01-06 15:56:50 +0300

    fixed set_window_size script

--- selenium_python_course/move_volume_slider.py
+++ selenium_python_course/move_volume_slider.py
@@ -9,20 +9,25 @@ url = "https://parsinger.ru/selenium/5.10/6/index.html"
 with webdriver.Chrome() as driver:
     driver.get(url)
     actions = ActionChains(driver=driver)
-    volume_slider = driver.find_element(By.XPATH, "//input[@class='volume-slider']")
-    slider_width = volume_slider.size['width']
-    print(slider_width)
-    offset = slider_width / 100
-    print(offset)
-    current_value = driver.find_element(By.XPATH, "//span[@class='current-value']").text
-    target_value = driver.find_element(By.XPATH, "//span[@class='target-value']").text
-    print(target_value)
-    if int(target_value) < int(current_value):
-        actions.click_and_hold(volume_slider).perform()
-        for i in range(int(target_value)):
-            actions.move_by_offset(offset, 0).perform()
-            time.sleep(1)
-    elif int(target_value) > int(current_value):
-        actions.click_and_hold(volume_slider).send_keys(Keys.ARROW_LEFT).perform()
-    time.sleep(5)
+    sliders = driver.find_elements(By.XPATH, "//div[@class='slider-container']")
+    for i in range(1, len(sliders) + 1):
+        current_value = int(driver.find_element(By.XPATH, f"//div[@class='slider-container'][{i}]/"
+                                                          f"span[@class='current-value']").text)
+        target_value = int(driver.find_element(By.XPATH, f"//div[@class='slider-container'][{i}]/"
+                                                         "span[@class='target-value']").text)
+        slider = driver.find_element(By.XPATH, f"//div[@class='slider-container'][{i}]/"
+                                               f"input[@class='volume-slider']")
+        if target_value < current_value:
+            diff = current_value - target_value
+            actions.click(slider).perform()
+            for j in range(diff):
+                actions.send_keys(Keys.ARROW_LEFT).perform()
+        elif target_value > current_value:
+            diff = target_value - current_value
+            actions.click(slider).perform()
+            for j in range(diff):
+                actions.send_keys(Keys.ARROW_RIGHT).perform()
+    result = driver.find_element(By.XPATH, "//p[@id='message']").text
+    print(result)
+    time.sleep(3)
 
