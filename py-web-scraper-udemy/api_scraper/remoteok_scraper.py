7431a38a38337d1bd03dddf366913f8cf02d0132
user <yasenovich.mikhail@mail.ru>
2025-01-23 17:25:36 +0300

    added py_web_scraper module

---
+++ py-web-scraper-udemy/api_scraper/remoteok_scraper.py
@@ -0,0 +1,20 @@
+import requests
+import xlwt
+from xlwt import Workbook
+import smtplib
+from os.path import basename
+from email.mime.application import MIMEApplication
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
+BASE_URL = 'https://remoteok.com/api/'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/127.0.0.0 Safari/537.36'
+
+
+def get_job_posting():
+    res = requests.get(url=BASE_URL)
+
+
+if __name__ == '__main__':
+    print("Hello")
